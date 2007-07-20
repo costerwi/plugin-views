d8a4331219f64999c2ff85e49745bc773f94037f
Carl Osterwisch <costerwi@gmail.com>
2007-07-20 18:15:46 +0000

    Initial revision

---
+++ views_plugin.py
@@ -0,0 +1,52 @@
+"""Register the Abaqus kernel methods from views.py"""
+
+import abaqusGui
+
+toolset = abaqusGui.getAFXApp().getAFXMainWindow().getPluginToolset()
+
+toolset.registerKernelMenuButton(
+        buttonText='&Views|&Synchronize', 
+        moduleName='views',
+        functionName='synchVps()',
+        author='Carl Osterwisch',
+        version='0.1',
+        applicableModules=['Visualization'],
+        description='Copy current viewport options to the others.')
+
+toolset.registerKernelMenuButton(
+        buttonText='&Views|&Cut plane normal to view', 
+        moduleName='views',
+        functionName='cutViewNormal()',
+        author='Carl Osterwisch',
+        version='0.1',
+        applicableModules=['Visualization'],
+        description='Cut normal to the current view.')
+
+toolset.registerKernelMenuButton(
+        buttonText='&Views|&View normal to cut plane', 
+        moduleName='views',
+        functionName='viewCutNormal()',
+        author='Carl Osterwisch',
+        version='0.1',
+        applicableModules=['Visualization'],
+        description='Orient view normal to current ' +
+                    'cutting plane.')
+
+toolset.registerKernelMenuButton(
+        buttonText='&Views|View &behind', 
+        moduleName='views',
+        functionName='behind()',
+        author='Carl Osterwisch',
+        version='0.1',
+        applicableModules=['Visualization'],
+        description='Flip the view 180 degrees (look behind)')
+
+toolset.registerKernelMenuButton(
+        buttonText='&Views|View &steps', 
+        moduleName='views',
+        functionName='viewSteps()',
+        author='Carl Osterwisch',
+        version='0.1',
+        applicableModules=['Visualization'],
+        description='Create a new viewport for each analysis step.')
+
